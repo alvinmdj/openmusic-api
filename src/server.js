35a09dbbaf40a8774c397c1cbb50025381adb0f6
Alvin <alvinmartindwrk@gmail.com>
2022-03-18 11:30:41 +0700

    initial server setup

---
+++ src/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // await server.register({
+
+  // });
+
+  await server.start();
+  console.log(`Server running on: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
