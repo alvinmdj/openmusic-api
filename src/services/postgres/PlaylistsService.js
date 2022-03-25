270d0207d80b7859cec8230d98cf6d00de30126b
Alvin <alvinmartindwrk@gmail.com>
2022-03-25 21:55:19 +0700

    create routes, handlers, and services for POST, GET, DELETE playlist

---
+++ src/services/postgres/PlaylistsService.js
@@ -0,0 +1,76 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylist({ name, owner }) {
+    const id = `playlist-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO playlists (id, name, owner) VALUES ($1, $2, $3) RETURNING id',
+      values: [id, name, owner],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Playlist could not be created');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getPlaylists(owner) {
+    const query = {
+      text: `
+        SELECT playlists.id, playlists.name, users.username FROM playlists
+        LEFT JOIN users ON playlists.owner = users.id
+        WHERE playlists.owner = $1
+      `,
+      values: [owner],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+
+  async deletePlaylistById(id) {
+    const query = {
+      text: 'DELETE FROM playlists WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to delete playlist. Id not found');
+    }
+  }
+
+  async verifyPlaylistOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to verify playlist owner. Id not found');
+    }
+
+    const playlist = result.rows[0];
+
+    if (playlist.owner !== owner) {
+      throw new AuthorizationError('You are not authorized to perform this action');
+    }
+  }
+}
+
+module.exports = PlaylistsService;
