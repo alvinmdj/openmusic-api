b906b811e8e651b25faeef848a3ac61d438b538a
Alvin <alvinmartindwrk@gmail.com>
2022-03-26 16:49:39 +0700

    create migration for creating playlist_song_activities table

---
+++ migrations/1648287472092_create-table-playlist-song-activities.js
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  // create playlist_song_activities table
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    action: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    time: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  // create foreign key constraint for playlist_id to id in playlists table
+  pgm.addConstraint('playlist_song_activities', 'fk_playlist_song_activities.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  // delete playlist_song_activities table
+  pgm.dropTable('playlist_song_activities');
+};
