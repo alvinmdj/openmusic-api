b8f8e252f025a102543e74e18ba68526b63c884b
Alvin <alvinmartindwrk@gmail.com>
2022-03-26 15:59:10 +0700

    create handler and service for collaborations

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -1,1 +1,41 @@
-// TODO: CREATE COLLABORATIONS SERVICE
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class CollaborationsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addCollaboration(playlistId, userId) {
+    const id = `collaboration-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO collaborations VALUES ($1, $2, $3) RETURNING id',
+      values: [id, playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Collaboration could not be created');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async deleteCollaboration(playlistId, userId) {
+    const query = {
+      text: 'DELETE FROM collaborations WHERE playlist_id = $1 AND user_id = $2 RETURNING id',
+      values: [playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Collaboration could not be deleted');
+    }
+  }
+}
+
+module.exports = CollaborationsService;
