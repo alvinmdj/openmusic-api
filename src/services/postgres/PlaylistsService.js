d3d77810dbea087b2456f3a7b818abaa9cae8145
Alvin <alvinmartindwrk@gmail.com>
2022-03-26 15:57:36 +0700

    add method to verify is playlist exist

--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -143,6 +143,19 @@ class PlaylistsService {
       throw new InvariantError('Failed to delete song from playlist');
     }
   }
+
+  async verifyPlaylistIsExist(id) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Playlist not found');
+    }
+  }
 }
 
 module.exports = PlaylistsService;
