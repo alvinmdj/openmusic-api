dd9c2bf4d5eb10ff1d1acb45110108a5e4e875c0
Alvin <alvinmartindwrk@gmail.com>
2022-03-26 15:58:57 +0700

    add auth options to collaborations routes

--- src/api/collaborations/routes.js
+++ src/api/collaborations/routes.js
@@ -3,11 +3,17 @@ const routes = (handler) => [
     method: 'POST',
     path: '/collaborations',
     handler: handler.postCollaborationHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/collaborations',
     handler: handler.deleteCollaborationHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
 ];
 
